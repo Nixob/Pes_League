7fff201a3906fa28517ea4abaa23fd9165f80835
Nixob <anmolkarki97@gmail.com>
2026-08-16 13:58:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -483,10 +483,4 @@ elif page_key == "admin":
 
 
 # ------------------------------------------------------------------- Footer --
-if page_key in ("register", "home"):
-    pass
-else:
-    st.markdown(
-        '<div class="footer-link">Not registered yet? Switch the dropdown above to <b>Register</b> ⚽</div>',
-        unsafe_allow_html=True,
-    )
+
